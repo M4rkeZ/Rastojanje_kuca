67f0b1c0a891d229663910434d2e26c7cfcd0e9d
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-09-28 09:30:14 +0200

    Add files via upload

---
+++ Rastojanje_kuca/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Rastojanje_kuca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1, v2, s1, s2, t, d, raz;
+
+            v1 = double.Parse(Console.ReadLine());
+            v2 = double.Parse(Console.ReadLine());
+            t = double.Parse(Console.ReadLine());
+            d = double.Parse(Console.ReadLine());
+
+            s1 = v1 * t;
+            s2 = v2 * t;
+
+            raz = (s2 + d) - s1;
+
+            Console.WriteLine(Math.Round(raz, 2));
+
+        }
+    }
+}
